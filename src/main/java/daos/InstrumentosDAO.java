d2ff2670cf0a982899aa9ad179280956862d6457
jgonzalez <jgonzalez@PCGS32.nelson.local>
2024-09-30 19:57:42 +0200

    primer

---
+++ src/main/java/daos/InstrumentosDAO.java
@@ -0,0 +1,13 @@
+package daos;
+
+import java.util.*;
+import modelo.*;
+
+public interface InstrumentosDAO {
+
+	List<Instrumento> obtenerInstrumentos();
+
+	void registrarInstrumento(Instrumento i);
+	void borrarInstrumento(int id);
+	void editarInstrumento(Instrumento i);
+}
